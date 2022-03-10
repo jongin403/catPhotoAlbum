6fca7e0b6a9bd0048042feb10642e30627351355
jongin.jeong <jongin403@naver.com>
2022-03-11 00:07:33 +0900

    Feat : breadcrumb 클릭 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,30 @@ export default function App($app) {
 
   const breadcrumb = new Breadcrumb({
     $app,
-    initialState: this.state.depth
+    initialState: [],
+    onClick: (index) => {
+      if (index === null) {
+        this.setState({
+          ...this.state,
+          depth: [],
+          nodes: cache.rootNodes
+        })
+        return
+      }
+  
+      if (index === this.state.depth.length - 1) {
+        return
+      }
+  
+      const nextState = { ...this.state }
+      const nextDepth = this.state.depth.slice(0, index + 1)
+  
+      this.setState({
+        ...nextState,
+        depth: nextDepth,
+        nodes: cache[nextDepth[nextDepth.length - 1].id]
+      })
+    }
   })
 
   const nodes = new Nodes({
--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,4 +1,4 @@
-export default function Breadcrumb({ $app, initialState }) {
+export default function Breadcrumb({ $app, initialState = [], onClick }) {
   this.state = initialState
 
   this.$target = document.createElement('nav')
@@ -10,11 +10,23 @@ export default function Breadcrumb({ $app, initialState }) {
     this.render()
   }
 
+  this.onClick = onClick
+
   this.render = () => {
     this.$target.innerHTML = `<div class="nav-item">root</div>${
-      this.state.map(
-        (node, index) => `<div class="nav-item" data-index="${index}">${node.name}</div>`).join('')}`
+      this.state.map((node, index) => 
+        `<div class="nav-item" data-index="${index}">${node.name}</div>`).join('')
+    }`
   }
+
+  this.$target.addEventListener('click', (e) => {
+    const $navItem = e.target.closest('.nav-item')
+
+    if ($navItem) {
+      const { index } = $navItem.dataset
+      this.onClick(index ? parseInt(index, 10) : null)
+    }
+  })
 
   this.render()
 }
